5f4e91063ff86aa87cbe629bf495c23ab5dd8d7d
Emmanuel Allan <emmanuelallanmj@gmail.com>
2022-09-17 11:39:16 +0530

    Added simple jsx code

---
+++ components/events/event-list.js
@@ -0,0 +1,22 @@
+import React from "react";
+import EventItems from "./event-items";
+
+function EventList(props) {
+  const { items } = props;
+  return (
+    <ul>
+      {items.map((event) => (
+        <EventItems
+          key={event.id}
+          id={event.id}
+          title={event.title}
+          location={event.location}
+          date={event.date}
+          image={event.image}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default EventList;
